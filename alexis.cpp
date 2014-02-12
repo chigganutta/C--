07a895e7a322f30ce64548d5fa62b5783722d4c0
Julian Lindemeier <lindemeier.julian@gmail.com>
2014-02-12 01:21:54 +0100

    Alexis is a flippin' douche

--- alexis.cpp
+++ alexis.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main(void) {
 	char var;
 	cout << "Was tut Alexis? " ;
-	var = cin.get();
+	//var = cin.get();
+	
+	cin << var;
 	cout << "Ja genau, er ist ein Meister im " << (char)var << '\n';
 	return 0;
 }
